d0fc7ae87ce3594dd0cee1376754f031ba600a28
eksup <eksup222@gmail.com>
2024-10-25 14:37:39 +0300

    Создание новой модели KNN

--- model/fit.py
+++ model/fit.py
@@ -2,12 +2,15 @@ import pickle
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import LabelEncoder
+labelencoder = LabelEncoder()
 
-iris_df = pd.read_csv("IRIS (1).csv")
-X = iris_df.drop(["species"], axis=1)
-Y = iris_df["species"]
+boot_df = pd.read_excel("HugeBoots.xlsx")
+boot_df.iloc[:, 2] = labelencoder.fit_transform(boot_df.iloc[:, 2])
+X = boot_df.drop(["Размер обуви"], axis=1)
+Y = boot_df["Размер обуви"]
 X_train1, X_test1, Y_train1, Y_test1 = train_test_split(X, Y, test_size=0.3, random_state=3)
 model = KNeighborsClassifier(n_neighbors=3)
 model.fit(X_train1, Y_train1)
-with open('Iris_pickle_file', 'wb') as pkl:
+with open('Boot_pickle_file', 'wb') as pkl:
     pickle.dump(model, pkl)
