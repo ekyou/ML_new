aa25d19839c4cd2dfca0c3409c91623cb81bef60
eksup <eksup222@gmail.com>
2024-10-25 19:34:19 +0300

    Добавление модели для лаб3

--- flsite.py
+++ flsite.py
@@ -11,6 +11,7 @@ menu = [{"name": "Лаба 1", "url": "p_knn"},
 
 loaded_model_knn = pickle.load(open('model/Sport_pickle_file', 'rb'))
 loaded_model_lab2 = pickle.load(open('model/Boots_pickle_file', 'rb'))
+loaded_model_lab3 = pickle.load(open('model/Credit_pickle_file', 'rb'))
 
 @app.route("/")
 def index():
@@ -30,24 +31,34 @@ def f_lab1():
         return render_template('lab1.html', title="Метод k -ближайших соседей (KNN)", menu=menu,
                                class_model=pred)
 
-@app.route("/p_lab2")
+@app.route("/p_lab2", methods=['POST', 'GET'])
 def f_lab2():
     if request.method == 'GET':
-        return render_template('lab1.html', title="Линейная регрессия для рассчета размера обуви", menu=menu, class_model='')
+        return render_template('lab2.html', title="Линейная регрессия для рассчета размера обуви", menu=menu, class_model='')
     if request.method == 'POST':
         X_new = np.array([[float(request.form['list1']),
                            float(request.form['list2']),
                            float(request.form['list3']),
                            ]])
         pred = loaded_model_lab2.predict(X_new)
-        return render_template('lab1.html', title="Линейная регрессия для рассчета размера обуви", menu=menu,
+        return render_template('lab2.html', title="Линейная регрессия для рассчета размера обуви", menu=menu,
                                class_model=pred)
 
 
-@app.route("/p_lab3")
+@app.route("/p_lab3", methods=['POST', 'GET'])
 def f_lab3():
-    return render_template('lab3.html', title="Логистическая регрессия", menu=menu)
-
+    if request.method == 'GET':
+        return render_template('lab3.html', title="Логистическая регрессия одобрения кредита", menu=menu,
+                               class_model='')
+    if request.method == 'POST':
+        X_new = np.array([[float(request.form['list1']),
+                           float(request.form['list2']),
+                           float(request.form['list3']),
+                           float(request.form['list4']),
+                           ]])
+        pred = loaded_model_lab3.predict(X_new)
+        return render_template('lab3.html', title="Логистическая регрессия одобрения кредита", menu=menu,
+                               class_model=pred)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- model/fit.py
+++ model/fit.py
@@ -3,7 +3,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.preprocessing import LabelEncoder
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression
+
 labelencoder = LabelEncoder()
 
 sport_df = pd.read_excel("sport.xlsx")
@@ -21,6 +22,16 @@ X2 = boots_df.iloc[:, :-1]
 Y2 = boots_df.iloc[:, -1]
 X_train2, X_test2, Y_train2, Y_text2 = train_test_split(X2, Y2, test_size=0.3, random_state=3)
 model = LinearRegression()
-model.fit(X_train2, Y_train2)
+model.fit(X_train2.values, Y_train2)
 with open('Boots_pickle_file', 'wb') as pkl:
+    pickle.dump(model, pkl)
+
+ct_df=pd.read_excel("credit.xlsx")
+ct_df.iloc[:, 4]=labelencoder.fit_transform(ct_df.iloc[:, 4])
+X3 = ct_df.iloc[:, :-1]
+Y3 = ct_df.iloc[:, -1]
+X_train3,X_test3,Y_train3,Y_test3 = train_test_split(X3,Y3,test_size=0.3,random_state=3)
+model = LogisticRegression(solver='liblinear', C=1.0)
+model.fit(X_train3,Y_train3)
+with open('Credit_pickle_file', 'wb') as pkl:
     pickle.dump(model, pkl)
